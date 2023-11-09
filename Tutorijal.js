a20471a5156627926d042be316cbd894ceb9dbb2
Nemanja Cvetković <so.cvejo03@gmail.com>
2023-11-09 23:23:31 +0100

    Add files via upload

--- Tutorijal.js
+++ Tutorijal.js
@@ -1,19 +1,31 @@
 
 var tasks=new Array();
-    for(var i=0;i<100;i++)tasks.push({Naslov:'', Text});
+    for(var i=0;i<100;i++)tasks.push({Naslov:'', Text:''});
 
     tasks[1].Naslov='Sabiranje';
-    tasks[1].Text='Da bi sabrao dva broja, probaj zapisati ih jedan ispod drugog';
-
     tasks[2].Naslov='Oduzimanje';
-    tasks[2].Text='Da bi sabrao dva broja, probaj zapisati ih jedan ispod drugog';
-
     tasks[3].Naslov='Množenje';
-    tasks[3].Text='Da bi sabrao dva broja, probaj zapisati ih jedan ispod drugog';
-
+    
     tasks[4].Naslov='Deljenje';
-    tasks[4].Text='Da bi sabrao dva broja, probaj zapisati ih jedan ispod drugog';
-
+    
+    tasks[5].Naslov='Nepoznati sabirak';
+    
+    
+    tasks[6].Naslov='Nepoznati umanjenik';
+    
+    
+    tasks[7].Naslov='Nepoznati umanjilac';
+    
+    
+    tasks[8].Naslov='Nepoznati činilac';
+    
+    
+    tasks[9].Naslov='Nepoznati deljenik';
+    
+    
+    tasks[10].Naslov='Nepoznati delilac';
+    
+    
 
 var istorijaIzrazi=new Array();
 
@@ -21,7 +33,11 @@ var istorijaIzrazi=new Array();
 function PromeniTutorijal(taskNumber){
     
     document.getElementById('NazivTutorijala').innerHTML=tasks[taskNumber].Naslov;
- 
+    document.getElementById('OpisTutorijala').innerHTML=tasks[taskNumber].Text;
+    document.getElementById('SlikaTutorijala1').src='./assets/Task'+taskNumber+'_1.svg';
+    document.getElementById('SlikaTutorijala2').src='./assets/Task'+taskNumber+'_2.svg';
+
+    
 }
 
 
